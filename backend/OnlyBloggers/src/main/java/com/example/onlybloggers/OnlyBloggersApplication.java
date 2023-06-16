e30576caf9d0dd14ca36ccec62befef27f29eb75
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-06-16 16:09:54 +0530

    backend setup

---
+++ backend/OnlyBloggers/src/main/java/com/example/onlybloggers/OnlyBloggersApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlybloggers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBloggersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyBloggersApplication.class, args);
+    }
+
+}
---
+++ backend/OnlyBloggers/src/test/java/com/example/onlybloggers/OnlyBloggersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlybloggers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyBloggersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
